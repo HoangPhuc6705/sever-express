5656864506103c4e83b0bbd652f314e76637b3d9
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2024-05-11 19:54:51 +0700

    sever

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+
+app.post('/crushs', (req, res) => {
+  const data = req.body;
+  console.log('Data received:', data);
+})
+
+app.get('/crush', (req, res) => {
+  const data = req.body;
+  res.send(data);
+})
+
+const PORT = 3000;
+app.listen(PORT, () => console.log('Sever is listening on PORT:', PORT));
+
+
